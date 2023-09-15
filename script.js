37ff1c42fbbcc2d2f13fcf63b575dca049d8d9e3
Kajal1907 <100417636+Kajal1907@users.noreply.github.com>
2023-09-15 16:25:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+	bar.addEventListener("click", () => {
+		nav.classList.remove("active");
+	});
+}
+if (close) {
+	close.addEventListener("click", () => {
+		nav.classList.remove("active");
+	});
+}
